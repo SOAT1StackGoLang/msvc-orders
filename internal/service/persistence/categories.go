f3da76bf5d30a46442c47f0a91ccd4e171da713d
George Baronheid <baronheid.george@gmail.com>
2024-01-25 21:25:51 -0300

    cats persistence done

--- internal/service/persistence/categories.go
+++ internal/service/persistence/categories.go
@@ -3,11 +3,10 @@ package persistence
 import (
 	"context"
 	"github.com/SOAT1StackGoLang/msvc-orders/internal/service/models"
+	kitlog "github.com/go-kit/log"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
-
-	kitlog "github.com/go-kit/log"
 )
 
 const categoriesTable = "lanchonete_categories"
@@ -18,8 +17,34 @@ type persistence struct {
 }
 
 func (p *persistence) InsertCategory(ctx context.Context, in *models.Category) (*models.Category, error) {
-	//TODO implement me
-	panic("implement me")
+	var out *models.Category
+	cat := Category{
+		ID:        in.ID,
+		CreatedAt: in.CreatedAt,
+		Name:      in.Name,
+	}
+
+	if err := p.db.WithContext(ctx).Table(categoriesTable).
+		Create(&cat).Error; err != nil {
+		p.log.Log(
+			"db failed inserting category",
+			zap.Any("in_category", in),
+			zap.Error(err),
+		)
+		return nil, err
+	}
+
+	out = &models.Category{
+		ID:        cat.ID,
+		CreatedAt: cat.CreatedAt,
+		Name:      cat.Name,
+	}
+
+	if cat.UpdatedAt.Valid {
+		out.CreatedAt = cat.UpdatedAt.Time
+	}
+
+	return out, nil
 }
 
 func (p *persistence) GetCategoryByID(ctx context.Context, id uuid.UUID) (*models.Category, error) {
@@ -49,15 +74,60 @@ func (p *persistence) GetCategoryByID(ctx context.Context, id uuid.UUID) (*model
 }
 
 func (p *persistence) DeleteCategory(ctx context.Context, id uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+	cat := Category{ID: id}
+	if err := p.db.WithContext(ctx).Table(categoriesTable).Delete(&cat).Error; err != nil {
+		p.log.Log(
+			"db failed deleting category",
+			zap.Any("category_id", id.String()),
+			zap.Error(err),
+		)
+		return err
+	}
+
+	return nil
 }
 
 func (p *persistence) ListCategories(ctx context.Context, limit int, offset int) (*models.CategoryList, error) {
-	//TODO implement me
-	panic("implement me")
+	var total int64
+	var savedCats []Category
+
+	var err error
+	if err = p.db.WithContext(ctx).Table(categoriesTable).
+		Limit(limit).
+		Offset(offset).
+		Count(&total).
+		Scan(&savedCats).Error; err != nil {
+		p.log.Log(
+			"failed listing categories",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+
+	out := &models.CategoryList{}
+	outList := make([]*models.Category, 0, total)
+
+	for _, c := range savedCats {
+		out := &models.Category{
+			ID:        c.ID,
+			CreatedAt: c.CreatedAt,
+			Name:      c.Name,
+		}
+
+		if c.UpdatedAt.Valid {
+			out.CreatedAt = c.UpdatedAt.Time
+		}
+		outList = append(outList, out)
+	}
+
+	out.Categories = outList
+	out.Total = total
+	out.Limit = limit
+	out.Offset = offset
+
+	return out, err
 }
 
-func NewCategoriesPersistence(ctx context.Context, db *gorm.DB, log kitlog.Logger) CategoriesRepository {
-	return &persistence{db: db}
+func NewCategoriesPersistence(db *gorm.DB, log kitlog.Logger) CategoriesRepository {
+	return &persistence{db: db, log: log}
 }
